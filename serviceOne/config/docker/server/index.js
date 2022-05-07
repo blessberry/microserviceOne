7d177662307d0211c346e54c71de0c999b8ab94c
blessberry <beraka.emmanuel@protonmail.ch>
2022-05-07 14:06:39 +0000

    Commit 5

--- serviceOne/config/docker/server/index.js
+++ serviceOne/config/docker/server/index.js
@@ -1,8 +1,8 @@
 const h = require('http');
-const PORT = 12345;
+const PORT = process.env.PORT;
 const s = h.createServer((q,r) => {
     r.writeHead(200, {'Content-Type': 'text/plain'});
-    r.end('1 Hello World.\n');
+    r.end('5 Hello World.\n');
 }).listen(PORT, () => {
     console.log(`Server Running on ${PORT}`);
 });
