4d9ed07bef20f20cb90e0067f6c83527a6c5ef14
blessberry <beraka.emmanuel@protonmail.ch>
2022-05-07 18:37:52 +0000

    Commit 9

--- serviceOne/src/one/index.js
+++ serviceOne/src/one/index.js
@@ -2,7 +2,7 @@ const h = require('http');
 const PORT = process.env.PORT;
 const s = h.createServer((q,r) => {
     r.writeHead(200, {'Content-Type': 'text/plain'});
-    r.end('8 Hello World.\n');
+    r.end('9 Hello World.\n');
 }).listen(PORT, () => {
     console.log(`Server Running on ${PORT}`);
 });
